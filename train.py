b9eff94113d8dd5c4383685d98734b7bc15e8a24
Hikaru Shioiri <j3339pie@sit.ac.jp>
2024-12-10 16:39:37 +0900

    5回に1回精度を測る

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ loss_fn = torch.nn.CrossEntropyLoss()
 learning_rate = 0.003
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-n_epochs = 5
+n_epochs = 20
 
 loss_train_history = []
 loss_test_history = []
@@ -71,16 +71,25 @@ for k in range(n_epochs):
     loss_train_history.append(loss_train)
     print(f'train loss: {loss_train:.3f} ({time_end-time_start:.1f}s)', end=',')
 
+    time_start = time.time()
     loss_test = models.test(model, dataloader_test, loss_fn)
+    time_end = time.time()
     loss_test_history.append(loss_test)
-    print(f'test loss: {loss_test:.3f}', end=',')
+    print(f'test loss: {loss_test:.3f} ({time_end-time_start:.1f}s)')
 
-    acc_train = models.test_accuracy(model, dataloader_test)
-    acc_train_history.append(acc_train)
-    print(f'train accuracy: {acc_train*100:.3f}%', end=',')
-    acc_test = models.test_accuracy(model, dataloader_test)
-    acc_test_history.append(acc_test)
-    print(f'test accuracy: {acc_test*100:.3f}%')
+    if (k+1) % 5 == 0:
+
+        time_start = time.time()
+        acc_train = models.test_accuracy(model, dataloader_test)
+        time_end = time.time()
+        acc_train_history.append(acc_train)
+        print(f'train accuracy: {acc_train*100:.3f}% ({time_end-time_start:.1f}s)', end=',')
+
+        time_start = time.time()
+        acc_test = models.test_accuracy(model, dataloader_test)
+        time_end = time.time()
+        acc_test_history.append(acc_test)
+        print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)')
 
 plt.plot(acc_train_history, label='train')
 plt.plot(acc_test_history, label='test')
